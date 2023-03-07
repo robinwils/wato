8099d389279e43eb0134e496aaff2994f1129b54
Robin WILS <robin.wilz@gmail.com>
2023-03-08 00:25:33 +0100

    add cubes example from bgfx

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <GLFW/glfw3.h>
 #include <bx/bx.h>
+#include <bx/math.h>
+#include <bx/allocator.h>
+#include <bx/file.h>
+#include <bx/debug.h>
 #include <bgfx/bgfx.h>
 #include <bgfx/platform.h>
 #if BX_PLATFORM_LINUX
@@ -12,11 +16,136 @@
 #endif
 #include <GLFW/glfw3native.h>
 
+#define DBG_STRINGIZE(_x) DBG_STRINGIZE_(_x)
+#define DBG_STRINGIZE_(_x) #_x
+#define DBG_FILE_LINE_LITERAL "" __FILE__ "(" DBG_STRINGIZE(__LINE__) "): "
+#define DBG(_format, ...) bx::debugPrintf(DBG_FILE_LINE_LITERAL "" _format "\n", ##__VA_ARGS__)
 
 static void glfw_keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
  //   if (key == GLFW_KEY_F1 && action == GLFW_RELEASE)
    //     s_showStats = !s_showStats;
+}
+
+struct PosColorVertex
+{
+    float m_x;
+    float m_y;
+    float m_z;
+    uint32_t m_abgr;
+
+    static void init()
+    {
+        ms_layout
+            .begin()
+            .add(bgfx::Attrib::Position, 3, bgfx::AttribType::Float)
+            .add(bgfx::Attrib::Color0, 4, bgfx::AttribType::Uint8, true)
+            .end();
+    };
+
+    static bgfx::VertexLayout ms_layout;
+};
+
+bgfx::VertexLayout PosColorVertex::ms_layout;
+
+static PosColorVertex s_cubeVertices[] =
+{
+    {-1.0f,  1.0f,  1.0f, 0xff000000 },
+    { 1.0f,  1.0f,  1.0f, 0xff0000ff },
+    {-1.0f, -1.0f,  1.0f, 0xff00ff00 },
+    { 1.0f, -1.0f,  1.0f, 0xff00ffff },
+    {-1.0f,  1.0f, -1.0f, 0xffff0000 },
+    { 1.0f,  1.0f, -1.0f, 0xffff00ff },
+    {-1.0f, -1.0f, -1.0f, 0xffffff00 },
+    { 1.0f, -1.0f, -1.0f, 0xffffffff },
+};
+
+static const uint16_t s_cubeTriList[] =
+{
+    0, 1, 2, // 0
+    1, 3, 2,
+    4, 6, 5, // 2
+    5, 6, 7,
+    0, 2, 4, // 4
+    4, 2, 6,
+    1, 5, 3, // 6
+    5, 7, 3,
+    0, 4, 1, // 8
+    4, 5, 1,
+    2, 3, 6, // 10
+    6, 3, 7,
+};
+
+bx::DefaultAllocator g_allocator;
+
+bx::FileReaderI* g_fileReader;
+bx::FileWriterI* g_fileWriter;
+
+
+static const bgfx::Memory* loadMem(bx::FileReaderI* _reader, const char* _filePath)
+{
+    if (bx::open(_reader, _filePath))
+    {
+        uint32_t size = (uint32_t)bx::getSize(_reader);
+        const bgfx::Memory* mem = bgfx::alloc(size + 1);
+        bx::read(_reader, mem->data, size, bx::ErrorAssert{});
+        bx::close(_reader);
+        mem->data[mem->size - 1] = '\0';
+        return mem;
+    }
+
+    DBG("Failed to load %s.", _filePath);
+    return NULL;
+}
+
+static bgfx::ShaderHandle loadShader(bx::FileReaderI* _reader, const char* _name)
+{
+    char filePath[512];
+
+    const char* shaderPath = "???";
+
+    switch (bgfx::getRendererType())
+    {
+    case bgfx::RendererType::Noop:
+    case bgfx::RendererType::Direct3D9:  shaderPath = "shaders/dx9/";   break;
+    case bgfx::RendererType::Direct3D11:
+    case bgfx::RendererType::Direct3D12: shaderPath = "shaders/dx11/";  break;
+    case bgfx::RendererType::Agc:
+    case bgfx::RendererType::Gnm:        shaderPath = "shaders/pssl/";  break;
+    case bgfx::RendererType::Metal:      shaderPath = "shaders/metal/"; break;
+    case bgfx::RendererType::Nvn:        shaderPath = "shaders/nvn/";   break;
+    case bgfx::RendererType::OpenGL:     shaderPath = "shaders/glsl/";  break;
+    case bgfx::RendererType::OpenGLES:   shaderPath = "shaders/essl/";  break;
+    case bgfx::RendererType::Vulkan:     shaderPath = "shaders/spirv/"; break;
+    case bgfx::RendererType::WebGPU:     shaderPath = "shaders/spirv/"; break;
+
+    case bgfx::RendererType::Count:
+        BX_ASSERT(false, "You should not be here!");
+        break;
+    }
+
+    bx::strCopy(filePath, BX_COUNTOF(filePath), shaderPath);
+    bx::strCat(filePath, BX_COUNTOF(filePath), _name);
+    bx::strCat(filePath, BX_COUNTOF(filePath), ".bin");
+
+    bgfx::ShaderHandle handle = bgfx::createShader(loadMem(_reader, filePath));
+    bgfx::setName(handle, _name);
+
+    return handle;
+}
+
+
+bgfx::ProgramHandle loadProgram(const char* _vsName, const char* _fsName)
+{
+    bgfx::ShaderHandle vsh = loadShader(g_fileReader, _vsName);
+    bgfx::ShaderHandle fsh = BGFX_INVALID_HANDLE;
+    if (NULL != _fsName)
+    {
+        fsh = loadShader(g_fileReader, _fsName);
+    }
+
+    return bgfx::createProgram(vsh, fsh, true /* destroy shaders when program is destroyed */);
+
 }
 
 int main()
@@ -65,7 +194,38 @@ int main()
     bgfx::setViewClear(kClearView, BGFX_CLEAR_COLOR);
     bgfx::setViewRect(kClearView, 0, 0, bgfx::BackbufferRatio::Equal);
     
-    // Enable debug text.
+    // Enable stats or debug text.
+    bgfx::setDebug(BGFX_DEBUG_TEXT);
+
+    // Set view 0 clear state.
+    bgfx::setViewClear(0
+        , BGFX_CLEAR_COLOR | BGFX_CLEAR_DEPTH
+        , 0x303030ff
+        , 1.0f
+        , 0
+    );
+
+    // Create vertex stream declaration.
+    PosColorVertex::init();
+
+    // Create static vertex buffer.
+    bgfx::VertexBufferHandle vbh = bgfx::createVertexBuffer(
+        // Static data can be passed with bgfx::makeRef
+        bgfx::makeRef(s_cubeVertices, sizeof(s_cubeVertices))
+        , PosColorVertex::ms_layout
+    );
+
+    // Create static index buffer for triangle list rendering.
+    bgfx::IndexBufferHandle ibh = bgfx::createIndexBuffer(
+        // Static data can be passed with bgfx::makeRef
+        bgfx::makeRef(s_cubeTriList, sizeof(s_cubeTriList))
+    );
+
+    g_fileReader = BX_NEW(&g_allocator, bx::FileReader);
+    g_fileWriter = BX_NEW(&g_allocator, bx::FileWriter);
+
+    bgfx::ProgramHandle program = loadProgram("vs_cubes", "fs_cubes");
+
 
     while (!glfwWindowShouldClose(window)) {
         glfwPollEvents();
@@ -86,8 +246,25 @@ int main()
         bgfx::dbgTextPrintf(80, 2, 0x0f, "\x1b[;8m    \x1b[;9m    \x1b[;10m    \x1b[;11m    \x1b[;12m    \x1b[;13m    \x1b[;14m    \x1b[;15m    \x1b[0m");
         const bgfx::Stats* stats = bgfx::getStats();
         bgfx::dbgTextPrintf(0, 2, 0x0f, "Backbuffer %dW x %dH in pixels, debug text %dW x %dH in characters.", stats->width, stats->height, stats->textWidth, stats->textHeight);
-        // Enable stats or debug text.
-        bgfx::setDebug(BGFX_DEBUG_TEXT);
+
+
+        const bx::Vec3 at = { 0.0f, 0.0f,   0.0f };
+        const bx::Vec3 eye = { 0.0f, 0.0f, -35.0f };
+
+
+        // Set view and projection matrix for view 0.
+        {
+            float view[16];
+            bx::mtxLookAt(view, eye, at);
+
+            float proj[16];
+            bx::mtxProj(proj, 60.0f, float(width) / float(height), 0.1f, 100.0f, bgfx::getCaps()->homogeneousDepth);
+            bgfx::setViewTransform(0, view, proj);
+
+            // Set view 0 default viewport.
+            bgfx::setViewRect(0, 0, 0, uint16_t(width), uint16_t(height));
+        }
+
         // Advance to next frame. Process submitted rendering primitives.
         bgfx::frame();
     }
