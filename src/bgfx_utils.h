2a04b602887d29b30ad3dc4667252441d65cc14b
Robin Wils <robin.wilz@gmail.com>
2023-04-30 15:20:15 +0200

    remove unused bgfx utils

--- src/bgfx_utils.h
+++ src/bgfx_utils.h
@@ -101,67 +101,6 @@ struct MeshState
 	bgfx::ViewId        m_viewId;
 };
 
-struct Primitive
-{
-	uint32_t m_startIndex;
-	uint32_t m_numIndices;
-	uint32_t m_startVertex;
-	uint32_t m_numVertices;
-
-	bx::Sphere m_sphere;
-	bx::Aabb   m_aabb;
-	bx::Obb    m_obb;
-};
-
-typedef stl::vector<Primitive> PrimitiveArray;
-
-struct Group
-{
-	Group();
-	void reset();
-
-	bgfx::VertexBufferHandle m_vbh;
-	bgfx::IndexBufferHandle m_ibh;
-	uint16_t m_numVertices;
-	uint8_t* m_vertices;
-	uint32_t m_numIndices;
-	uint16_t* m_indices;
-	bx::Sphere m_sphere;
-	bx::Aabb   m_aabb;
-	bx::Obb    m_obb;
-	PrimitiveArray m_prims;
-};
-typedef stl::vector<Group> GroupArray;
-
-struct Mesh
-{
-	void load(bx::ReaderSeekerI* _reader, bool _ramcopy);
-	void unload();
-	void submit(bgfx::ViewId _id, bgfx::ProgramHandle _program, const float* _mtx, uint64_t _state) const;
-	void submit(const MeshState*const* _state, uint8_t _numPasses, const float* _mtx, uint16_t _numMatrices) const;
-
-	bgfx::VertexLayout m_layout;
-	GroupArray m_groups;
-};
-/*
-///
-Mesh* meshLoad(const char* _filePath, bool _ramcopy = false);
-
-///
-void meshUnload(Mesh* _mesh);
-
-///
-MeshState* meshStateCreate();
-
-///
-void meshStateDestroy(MeshState* _meshState);
-
-///
-void meshSubmit(const Mesh* _mesh, bgfx::ViewId _id, bgfx::ProgramHandle _program, const float* _mtx, uint64_t _state = BGFX_STATE_MASK);
-
-///
-void meshSubmit(const Mesh* _mesh, const MeshState*const* _state, uint8_t _numPasses, const float* _mtx, uint16_t _numMatrices = 1);
-*/
 /// bgfx::RendererType::Enum to name.
 bx::StringView getName(bgfx::RendererType::Enum _type);
 
