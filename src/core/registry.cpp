f3e3c686a4cf29cfab9469957da202bd38a93fc4
Robin WILS <robin.wilz@gmail.com>
2023-05-14 16:40:42 +0200

    registry: load textures and add material to plane

--- src/core/registry.cpp
+++ src/core/registry.cpp
@@ -8,11 +8,14 @@
 
 void Registry::spawnPlane()
 {
-	bgfx::ProgramHandle program = loadProgram(m_bxFactory.getDefaultFileReader(), "vs_cubes", "fs_cubes");
+	//bgfx::ProgramHandle program = loadProgram(&BxFactory::getInstance().reader, "vs_cubes", "fs_cubes");
+	bgfx::ProgramHandle program = loadProgram(&BxFactory::getInstance().reader, "vs_blinnphong", "fs_blinnphong");
+	bgfx::TextureHandle diffuse = loadTexture("assets/textures/TreeTop_COLOR.png");
+	bgfx::TextureHandle specular = loadTexture("assets/textures/TreeTop_SPEC.png");
 
 	auto plane = create();
 	emplace<Position>(plane, glm::vec3(0.0f));
 	emplace<Rotation>(plane, glm::vec3(0.0f));
-	emplace<Scale>(plane, glm::vec3(4.0f));
-	emplace<SceneObject>(plane, new PlanePrimitive(), Material());
+	emplace<Scale>(plane, glm::vec3(1.0f));
+	emplace<SceneObject>(plane, new PlanePrimitive(), Material(program, diffuse, specular));
 }
--- src/core/registry.hpp
+++ src/core/registry.hpp
@@ -5,8 +5,5 @@
 struct Registry : public entt::basic_registry<entt::entity>
 {
 	void spawnPlane();
-
-private:
-	BxFactory m_bxFactory;
 };
 
