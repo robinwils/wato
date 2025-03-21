437f5778ec502f2c5143812be8f333bfab8b06a1
Robin Wils <robin.wilz@gmail.com>
2025-03-21 18:13:13 +0100

    components: remove unused pos rot and scale

--- src/components/position.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <glm/ext/vector_float3.hpp>
-
-struct Position {
-    glm::vec3 position;
-};
--- src/components/rotation.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <glm/ext/vector_float3.hpp>
-
-struct Rotation {
-    glm::vec3 rotation;
-};
