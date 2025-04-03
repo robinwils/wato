7f50deaae6323cb3d1aaa8d155bb504561933b87
Robin Wils <robin.wilz@gmail.com>
2025-04-03 23:11:13 +0200

    core: add ring_buffer tests

--- src/core/queue/ring_buffer.hpp
+++ src/core/queue/ring_buffer.hpp
@@ -125,3 +125,54 @@ struct RingBuffer {
     uint32_t              mPrevious;
     bx::RingBufferControl mCtrl;
 };
+
+#include <string>
+
+#include "doctest.h"
+
+TEST_CASE("ring_buffer.simple_struct")
+{
+    struct SimpleStruct {
+        SimpleStruct() : A(-1), B("") {}
+        int         A;
+        std::string B;
+    };
+
+    RingBuffer<SimpleStruct, 8> rb;
+
+    CHECK(!rb.Previous());
+    rb.Latest().A = 42;
+    rb.Latest().B = "42";
+    rb.Next();
+
+    rb.Latest().A = 43;
+    rb.Latest().B = "43";
+    rb.Next();
+
+    rb.Latest().A = 44;
+    rb.Latest().B = "44";
+
+    SimpleStruct&                s  = rb.Latest();
+    SimpleStruct&                so = rb.Oldest();
+    std::optional<SimpleStruct>& sp = rb.Previous();
+
+    CHECK_EQ(s.A, 44);
+    CHECK_EQ(so.A, 42);
+    CHECK(sp);
+    CHECK_EQ(sp->A, 43);
+}
+
+TEST_CASE("ring_buffer.round_trip")
+{
+    RingBuffer<uint32_t, 8> rb;
+
+    for (uint32_t i = 0; i < rb.CAPACITY * 2; ++i) {
+        rb.Latest() = i;
+        CHECK_EQ(rb.Latest(), i);
+        if (i != 0) {
+            CHECK(rb.Previous());
+            CHECK_EQ(*rb.Previous(), i - 1);
+        }
+        rb.Next();
+    }
+}
