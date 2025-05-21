31aea3ab5d656cb62eaa6aa1e01e7fbccfe0174d
Robin Wils <robin.wilz@gmail.com>
2025-05-21 23:36:04 +0200

    renderer: add plane primitive copy and move constructors

--- src/renderer/plane_primitive.hpp
+++ src/renderer/plane_primitive.hpp
@@ -14,6 +14,11 @@
 class PlanePrimitive : public Primitive<PositionNormalUvVertex>
 {
    public:
+    PlanePrimitive(const PlanePrimitive&)            = delete;
+    PlanePrimitive(PlanePrimitive&&)                 = default;
+    PlanePrimitive& operator=(const PlanePrimitive&) = delete;
+    PlanePrimitive& operator=(PlanePrimitive&&)      = default;
+
     explicit PlanePrimitive(Material* aMaterial) : Primitive<PositionNormalUvVertex>(aMaterial)
     {
         mVertices = {
