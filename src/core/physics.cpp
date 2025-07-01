241d45ab1f1f8401f1a5abc16d5165dd33fe8467
Robin Wils <robin.wilz@gmail.com>
2025-07-01 10:36:28 +0200

    physics: fix undefined uint on windows

--- src/core/physics.cpp
+++ src/core/physics.cpp
@@ -22,15 +22,15 @@ void Physics::Init(Registry& aRegistry)
 
 void Physics::InitLogger()
 {
-    uint logLevel = 0;
+    uint32_t logLevel = 0;
     if (Params.InfoLogs) {
-        logLevel |= static_cast<uint>(rp3d::Logger::Level::Information);
+        logLevel |= static_cast<uint32_t>(rp3d::Logger::Level::Information);
     }
     if (Params.WarningLogs) {
-        logLevel |= static_cast<uint>(rp3d::Logger::Level::Warning);
+        logLevel |= static_cast<uint32_t>(rp3d::Logger::Level::Warning);
     }
     if (Params.ErrorLogs) {
-        logLevel |= static_cast<uint>(rp3d::Logger::Level::Error);
+        logLevel |= static_cast<uint32_t>(rp3d::Logger::Level::Error);
     }
 
     // Output the logs into an HTML file
