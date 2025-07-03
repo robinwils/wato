c4524973a883d5e1dd3faa8a1b3b3687cd0e888b
Robin Wils <robin.wilz@gmail.com>
2025-07-03 02:27:15 +0200

    cmake: generate shaders as embedded header files

--- src/resource/shader_loader.cpp
+++ src/resource/shader_loader.cpp
@@ -1,90 +1,29 @@
 #include "shader_loader.hpp"
 
+#include <bgfx/bgfx.h>
+
 #include "core/sys/log.hpp"
 #include "core/sys/mem.hpp"
-
-static bgfx::ShaderHandle loadShader(bx::FileReaderI* aReader, const char* aName)
-{
-    char filePath[512];
-
-    const char* renderer = "???";
-
-    bx::strCopy(filePath, BX_COUNTOF(filePath), "shaders/");
-    bx::strCat(filePath, BX_COUNTOF(filePath), aName);
-
-#if BX_PLATFORM_WINDOWS
-    bx::strCat(filePath, BX_COUNTOF(filePath), "_windows");
-#elif BX_PLATFORM_LINUX
-    bx::strCat(filePath, BX_COUNTOF(filePath), "_linux");
-#elif BX_PLATFORM_OSX
-    bx::strCat(filePath, BX_COUNTOF(filePath), "_osx");
-#endif
-
-    switch (bgfx::getRendererType()) {
-        case bgfx::RendererType::Noop:
-        case bgfx::RendererType::Direct3D11:
-        case bgfx::RendererType::Direct3D12:
-            renderer = "_dx11";
-            break;
-        case bgfx::RendererType::Agc:
-        case bgfx::RendererType::Gnm:
-            renderer = "_pssl";
-            break;
-        case bgfx::RendererType::Metal:
-            renderer = "_metal";
-            break;
-        case bgfx::RendererType::Nvn:
-            renderer = "_nvn";
-            break;
-        case bgfx::RendererType::OpenGL:
-            renderer = "_glsl";
-            break;
-        case bgfx::RendererType::OpenGLES:
-            renderer = "_essl";
-            break;
-        case bgfx::RendererType::Vulkan:
-            renderer = "_spirv";
-            break;
-
-        case bgfx::RendererType::Count:
-            BX_ASSERT(false, "You should not be here!");
-            break;
-    }
-
-    bx::strCat(filePath, BX_COUNTOF(filePath), renderer);
-    bx::strCat(filePath, BX_COUNTOF(filePath), ".bin");
-
-    bgfx::ShaderHandle handle = bgfx::createShader(loadMem(aReader, filePath));
-    bgfx::setName(handle, aName);
-
-    return handle;
-}
-
-static bgfx::ProgramHandle
-loadProgram(bx::FileReader* aFr, const char* aVsName, const char* aFsName)
-{
-    bgfx::ShaderHandle vsh = loadShader(aFr, aVsName);
-    bgfx::ShaderHandle fsh = BGFX_INVALID_HANDLE;
-    if (NULL != aFsName) {
-        fsh = loadShader(aFr, aFsName);
-    }
-
-    return bgfx::createProgram(vsh, fsh, true /* destroy shaders when program is destroyed */);
-}
 
 ShaderLoader::result_type ShaderLoader::operator()(
     const char*             aVsName,
     const char*             aFsName,
     const uniform_desc_map& aUniforms)
 {
-    bx::FileReader fr;
-    auto           handle = loadProgram(&fr, aVsName, aFsName);
-
     auto uniformHandles = Shader::uniform_map{};
     for (auto&& [name, desc] : aUniforms) {
         DBG("creating uniform '{}'", name);
         uniformHandles[name] = bgfx::createUniform(name.c_str(), desc.Type, desc.Number);
     }
 
-    return std::make_shared<Shader>(handle, uniformHandles);
+    bx::FileReader           fr;
+    bgfx::RendererType::Enum type = bgfx::getRendererType();
+
+    bgfx::ShaderHandle vsh = bgfx::createEmbeddedShader(kEmbeddedShaders, type, aVsName);
+    bgfx::ShaderHandle fsh = BGFX_INVALID_HANDLE;
+    if (aFsName != nullptr) {
+        fsh = bgfx::createEmbeddedShader(kEmbeddedShaders, type, aFsName);
+    }
+
+    return std::make_shared<Shader>(bgfx::createProgram(vsh, fsh, true), uniformHandles);
 }
--- src/resource/shader_loader.hpp
+++ src/resource/shader_loader.hpp
@@ -1,17 +1,50 @@
 #pragma once
 
+#include <bgfx/embedded_shader.h>
 #include <bx/bx.h>
 #include <bx/file.h>
+#include <essl/fs_blinnphong.sc.bin.h>
+#include <essl/vs_blinnphong.sc.bin.h>
+#include <essl/vs_blinnphong_skinned.sc.bin.h>
+#include <glsl/fs_blinnphong.sc.bin.h>
+#include <glsl/vs_blinnphong.sc.bin.h>
+#include <glsl/vs_blinnphong_skinned.sc.bin.h>
+#include <spirv/fs_blinnphong.sc.bin.h>
+#include <spirv/vs_blinnphong.sc.bin.h>
+#include <spirv/vs_blinnphong_skinned.sc.bin.h>
 
 #include <memory>
 #include <unordered_map>
 
 #include "renderer/shader.hpp"
+#if defined(_WIN32)
+#include <dx11/fs_blinnphong.sc.bin.h>
+#include <dx11/vs_blinnphong.sc.bin.h>
+#include <dx11/vs_blinnphong_skinned.sc.bin.h>
+#else
+// BX marks linux as a supported platform for Direct3D shaders but shaderc CMake wrapper does not
+// generate shader headers for Direct3D on Linux so we need to override the following macro
+#define BGFX_EMBEDDED_SHADER_DXBC(...)
+#endif  //  defined(_WIN32)
+#if __APPLE__
+#include <metal/fs_blinnphong.sc.bin.h>
+#include <metal/vs_blinnphong.sc.bin.h>
+#include <metal/vs_blinnphong_skinned.sc.bin.h>
+#endif  // __APPLE__
+        //
+
+static const bgfx::EmbeddedShader kEmbeddedShaders[] = {
+    BGFX_EMBEDDED_SHADER(vs_blinnphong),
+    BGFX_EMBEDDED_SHADER(vs_blinnphong_skinned),
+    BGFX_EMBEDDED_SHADER(fs_blinnphong),
+    BGFX_EMBEDDED_SHADER_END()};
 
 struct ShaderLoader final {
     using uniform_desc_map = std::unordered_map<std::string, UniformDesc>;
     using result_type      = std::shared_ptr<Shader>;
 
-    result_type
-    operator()(const char* aVsName, const char* aFsName, const uniform_desc_map& aUniforms);
+    result_type operator()(
+        const char*             aVsName,
+        const char*             aFsName   = "",
+        const uniform_desc_map& aUniforms = {});
 };
