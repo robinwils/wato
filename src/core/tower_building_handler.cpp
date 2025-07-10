7b0e4e5d42e9e91ee7ed4c991f93d001ba61b542
Robin Wils <robin.wilz@gmail.com>
2025-07-11 01:10:31 +0200

    core: add contact points to tower building handler

--- src/core/tower_building_handler.cpp
+++ src/core/tower_building_handler.cpp
@@ -8,10 +8,27 @@ void TowerBuildingHandler::onContact(const rp3d::CollisionCallback::CallbackData
 {
     for (uint32_t pairIdx = 0; pairIdx < aCallbackData.getNbContactPairs(); ++pairIdx) {
         rp3d::CollisionCallback::ContactPair pair = aCallbackData.getContactPair(pairIdx);
-        if (pair.getCollider1()->getCollisionCategoryBits() == Category::Entities
-            || pair.getCollider2()->getCollisionCategoryBits() == Category::Entities) {
+
+        unsigned short c1Category = pair.getCollider1()->getCollisionCategoryBits();
+        unsigned short c2Category = pair.getCollider2()->getCollisionCategoryBits();
+
+        spdlog::debug(
+            "got contact pair {} with {} contact points: 0x{:x}-0x{:x}",
+            pairIdx,
+            pair.getNbContactPoints(),
+            c1Category,
+            c2Category);
+        if (c1Category == Category::Entities || c2Category == Category::Entities) {
             CanBuildTower = true;
-            break;
+        }
+        if (c1Category == Category::Terrain || c2Category == Category::Terrain) {
+            for (uint32_t ptIdx = 0; ptIdx < pair.getNbContactPoints(); ++ptIdx) {
+                rp3d::CollisionCallback::ContactPoint point = pair.getContactPoint(ptIdx);
+                rp3d::Vector3 worldPoint = pair.getCollider1()->getLocalToWorldTransform()
+                                           * point.getLocalPointOnCollider1();
+                spdlog::debug("  got contact point {}: {}", ptIdx, worldPoint);
+                Contacts.emplace_back(worldPoint.x, worldPoint.y, worldPoint.z);
+            }
         }
     }
 }
