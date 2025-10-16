4640f406f4862665b520e399d5dfe634170c3f32
Robin Wils <2084390+robinwils@users.noreply.github.com>
2025-10-16 18:56:18 +0200

    registry: add storage diff WIP

--- src/registry/registry.hpp
+++ src/registry/registry.hpp
@@ -3,3 +3,21 @@
 #include "entt/entity/registry.hpp"
 
 using Registry = entt::basic_registry<entt::entity>;
+
+template <typename Cmp>
+struct ComponentDiff {
+    Cmp State;
+    Cmp Registry;
+};
+
+template <typename Cmp>
+entt::storage<ComponentDiff<Cmp>> DiffCmpWithNamedPool(
+    Registry&           aRegistry,
+    const entt::id_type aStorageID)
+{
+    auto  regView = aRegistry.view<Cmp>();
+    auto& aa      = aRegistry.storage<Cmp>(aStorageID);
+
+    for (auto entity : regView) {
+    }
+}
