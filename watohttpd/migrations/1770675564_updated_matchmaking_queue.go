0fe26affdf6c8cc21262da5378b20491d9a0f30e
Robin Wils <2084390+robinwils@users.noreply.github.com>
2026-02-10 18:37:48 +0100

    migrations: add unique constraint on account for matchmaking queue

---
+++ watohttpd/migrations/1770675564_updated_matchmaking_queue.go
@@ -0,0 +1,42 @@
+package migrations
+
+import (
+	"encoding/json"
+
+	"github.com/pocketbase/pocketbase/core"
+	m "github.com/pocketbase/pocketbase/migrations"
+)
+
+func init() {
+	m.Register(func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_3140092784")
+		if err != nil {
+			return err
+		}
+
+		// update collection data
+		if err := json.Unmarshal([]byte(`{
+			"indexes": [
+				"CREATE UNIQUE INDEX ` + "`" + `idx_HS4MixyI37` + "`" + ` ON ` + "`" + `matchmaking_queue` + "`" + ` (` + "`" + `accountName` + "`" + `)"
+			]
+		}`), &collection); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	}, func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_3140092784")
+		if err != nil {
+			return err
+		}
+
+		// update collection data
+		if err := json.Unmarshal([]byte(`{
+			"indexes": []
+		}`), &collection); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	})
+}
