e8ed67abfd086abb45613e594d9bb86587715187
Robin Wils <2084390+robinwils@users.noreply.github.com>
2026-02-10 18:36:16 +0100

    migrations: add game relation to matchmaking queue

---
+++ watohttpd/migrations/1770674771_updated_matchmaking_queue.go
@@ -0,0 +1,58 @@
+package migrations
+
+import (
+	"github.com/pocketbase/pocketbase/core"
+	m "github.com/pocketbase/pocketbase/migrations"
+)
+
+func init() {
+	m.Register(func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_3140092784")
+		if err != nil {
+			return err
+		}
+
+		// update field
+		if err := collection.Fields.AddMarshaledJSONAt(6, []byte(`{
+			"cascadeDelete": false,
+			"collectionId": "pbc_69621308",
+			"hidden": false,
+			"id": "relation1542800728",
+			"maxSelect": 1,
+			"minSelect": 0,
+			"name": "game",
+			"presentable": false,
+			"required": false,
+			"system": false,
+			"type": "relation"
+		}`)); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	}, func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_3140092784")
+		if err != nil {
+			return err
+		}
+
+		// update field
+		if err := collection.Fields.AddMarshaledJSONAt(6, []byte(`{
+			"cascadeDelete": false,
+			"collectionId": "pbc_69621308",
+			"hidden": false,
+			"id": "relation1542800728",
+			"maxSelect": 1,
+			"minSelect": 0,
+			"name": "field",
+			"presentable": false,
+			"required": false,
+			"system": false,
+			"type": "relation"
+		}`)); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	})
+}
