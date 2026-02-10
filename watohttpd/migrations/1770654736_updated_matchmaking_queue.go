5aa9de2f9613c5989b516b99e4ed96ced412bd60
Robin Wils <2084390+robinwils@users.noreply.github.com>
2026-02-10 18:30:44 +0100

    migrations: add matchmaking_queue status field as select

---
+++ watohttpd/migrations/1770654736_updated_matchmaking_queue.go
@@ -0,0 +1,66 @@
+package migrations
+
+import (
+	"github.com/pocketbase/pocketbase/core"
+	m "github.com/pocketbase/pocketbase/migrations"
+)
+
+func init() {
+	m.Register(func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_3140092784")
+		if err != nil {
+			return err
+		}
+
+		// remove field
+		collection.Fields.RemoveById("text2063623452")
+
+		// add field
+		if err := collection.Fields.AddMarshaledJSONAt(3, []byte(`{
+			"hidden": false,
+			"id": "select2063623452",
+			"maxSelect": 1,
+			"name": "status",
+			"presentable": false,
+			"required": true,
+			"system": false,
+			"type": "select",
+			"values": [
+				"waiting",
+				"matched"
+			]
+		}`)); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	}, func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_3140092784")
+		if err != nil {
+			return err
+		}
+
+		// add field
+		if err := collection.Fields.AddMarshaledJSONAt(2, []byte(`{
+			"autogeneratePattern": "",
+			"hidden": false,
+			"id": "text2063623452",
+			"max": 0,
+			"min": 0,
+			"name": "status",
+			"pattern": "",
+			"presentable": false,
+			"primaryKey": false,
+			"required": false,
+			"system": false,
+			"type": "text"
+		}`)); err != nil {
+			return err
+		}
+
+		// remove field
+		collection.Fields.RemoveById("select2063623452")
+
+		return app.Save(collection)
+	})
+}
