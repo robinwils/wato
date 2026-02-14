4918d6cf554bbb2ee8391a80ba538489b3c180e8
Robin Wils <2084390+robinwils@users.noreply.github.com>
2026-02-14 12:09:40 +0100

    player: convert PB ID to uint32_t

--- src/components/player.hpp
+++ src/components/player.hpp
@@ -1,10 +1,25 @@
 #pragma once
 
+#include <charconv>
 #include <cstdint>
 #include <entt/entity/fwd.hpp>
+#include <expected>
 #include <string>
 
 using PlayerID = uint32_t;
+
+inline std::expected<PlayerID, std::errc> PlayerIDFromHexString(const std::string& aHexStr)
+{
+    if (aHexStr.empty()) {
+        return std::unexpected(std::errc::invalid_argument);
+    }
+    PlayerID id{};
+    auto [ptr, ec] = std::from_chars(aHexStr.data(), aHexStr.data() + aHexStr.size(), id, 16);
+    if (ec == std::errc{}) {
+        return id;
+    }
+    return std::unexpected(ec);
+}
 
 struct Player {
     PlayerID ID;
