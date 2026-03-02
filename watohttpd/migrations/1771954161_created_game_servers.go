e0c55d2126b8373be57fbc76ff2d6bdbe640c433
Robin Wils <2084390+robinwils@users.noreply.github.com>
2026-03-02 18:55:16 +0100

    migrations: added game_servers collection

---
+++ watohttpd/migrations/1771954161_created_game_servers.go
@@ -0,0 +1,103 @@
+package migrations
+
+import (
+	"encoding/json"
+
+	"github.com/pocketbase/pocketbase/core"
+	m "github.com/pocketbase/pocketbase/migrations"
+)
+
+func init() {
+	m.Register(func(app core.App) error {
+		jsonData := `{
+			"createRule": null,
+			"deleteRule": null,
+			"fields": [
+				{
+					"autogeneratePattern": "[a-f0-9]{4}",
+					"hidden": false,
+					"id": "text3208210256",
+					"max": 4,
+					"min": 4,
+					"name": "id",
+					"pattern": "^[a-z0-9]+$",
+					"presentable": false,
+					"primaryKey": true,
+					"required": true,
+					"system": true,
+					"type": "text"
+				},
+				{
+					"autogeneratePattern": "",
+					"hidden": false,
+					"id": "text2783163181",
+					"max": 15,
+					"min": 0,
+					"name": "ip",
+					"pattern": "^\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}$",
+					"presentable": false,
+					"primaryKey": false,
+					"required": false,
+					"system": false,
+					"type": "text"
+				},
+				{
+					"autogeneratePattern": "",
+					"hidden": false,
+					"id": "text1630053352",
+					"max": 44,
+					"min": 44,
+					"name": "publicKey",
+					"pattern": "^[A-Za-z0-9+/]{43}=$",
+					"presentable": false,
+					"primaryKey": false,
+					"required": true,
+					"system": false,
+					"type": "text"
+				},
+				{
+					"hidden": false,
+					"id": "autodate2990389176",
+					"name": "created",
+					"onCreate": true,
+					"onUpdate": false,
+					"presentable": false,
+					"system": false,
+					"type": "autodate"
+				},
+				{
+					"hidden": false,
+					"id": "autodate3332085495",
+					"name": "updated",
+					"onCreate": true,
+					"onUpdate": true,
+					"presentable": false,
+					"system": false,
+					"type": "autodate"
+				}
+			],
+			"id": "pbc_2379271902",
+			"indexes": [],
+			"listRule": "",
+			"name": "game_servers",
+			"system": false,
+			"type": "base",
+			"updateRule": null,
+			"viewRule": ""
+		}`
+
+		collection := &core.Collection{}
+		if err := json.Unmarshal([]byte(jsonData), &collection); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	}, func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_2379271902")
+		if err != nil {
+			return err
+		}
+
+		return app.Delete(collection)
+	})
+}
