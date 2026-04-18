923ab82f7682b24866d8f69044a3bcf5646d7e94
Robin Wils <2084390+robinwils@users.noreply.github.com>
2026-04-18 11:31:00 +0200

    test: add more fixture methods

--- test/test_fixtures.hpp
+++ test/test_fixtures.hpp
@@ -4,9 +4,14 @@
 
 #include <glaze/glaze.hpp>
 
+#include "components/creep.hpp"
+#include "components/game.hpp"
+#include "components/health.hpp"
 #include "components/player.hpp"
 #include "components/rigid_body.hpp"
+#include "components/spawner.hpp"
 #include "components/tower.hpp"
+#include "components/transform3d.hpp"
 #include "core/gameplay_definitions.hpp"
 #include "core/graph.hpp"
 #include "core/physics/physics.hpp"
@@ -22,6 +27,7 @@ struct RegistryFixture {
     Physics&             Phy       = Reg.ctx().emplace<Physics>(Log);
     GameStateBuffer&     Buf       = Reg.ctx().emplace<GameStateBuffer>();
     FixedSystemExecutor& FixedExec = Reg.ctx().emplace<FixedSystemExecutor>();
+    GameInstance&        Instance  = Reg.ctx().emplace<GameInstance>();
     GameplayDef          Definitions;
 
     RegistryFixture()
@@ -38,11 +44,14 @@ struct RegistryFixture {
         Phy.Init();
     }
 
-    entt::entity AddPlayer(PlayerID aID, uint8_t aSlot)
+    entt::entity AddPlayer(PlayerID aID, uint8_t aSlot, int aGold = -1, float aHealth = 10.0f)
     {
-        auto e = Reg.create();
-        Reg.emplace<Player>(e, aID, aSlot);
-        return e;
+        auto player = Reg.create();
+        Reg.emplace<Player>(player, aID, aSlot);
+        Reg.emplace<Gold>(player, aGold >= 0 ? aGold : Definitions.Economy.StartingGold);
+        Reg.emplace<Health>(player, aHealth);
+        Reg.emplace<Transform3D>(player, glm::vec3(2.0f, 0.0f, 2.0f));
+        return player;
     }
 
     template <typename... Systems>
@@ -51,7 +60,13 @@ struct RegistryFixture {
         (FixedExec.Register<Systems>(), ...);
     }
 
-    void Tick(uint32_t aTick = 0) { FixedExec.Update(aTick, &Reg); }
+    void RunTicks(uint32_t aCount = 1)
+    {
+        for (uint32_t i = 0; i < aCount; ++i) {
+            ++Instance.Tick;
+            FixedExec.Update(Instance.Tick, &Reg);
+        }
+    }
 };
 
 struct ClientFixture : RegistryFixture {
@@ -78,8 +93,10 @@ struct ClientFixture : RegistryFixture {
 };
 
 struct ServerFixture : RegistryFixture {
-    TaggedActionsType& Tagged = Reg.ctx().emplace<TaggedActionsType>();
-    PlayerGraphMap&    Graphs = Reg.ctx().emplace<PlayerGraphMap>();
+    TaggedActionsType&     Tagged  = Reg.ctx().emplace<TaggedActionsType>();
+    PlayerGraphMap&        Graphs  = Reg.ctx().emplace<PlayerGraphMap>();
+    CommonIncome&          Income  = Reg.ctx().emplace<CommonIncome>(Definitions.Economy.StartingIncome);
+    std::vector<PlayerID>& Ranking = Reg.ctx().emplace_as<std::vector<PlayerID>>("ranking"_hs);
 
     void AddPlayerGraph(PlayerID aID, float aMapSize = 20.0f)
     {
@@ -88,5 +105,22 @@ struct ServerFixture : RegistryFixture {
             aMapSize * GraphCell::kCellsPerAxis,
             aMapSize * GraphCell::kCellsPerAxis,
             glm::vec2{0.0f, 0.0f});
+    }
+
+    entt::entity AddSpawner(PlayerID aOwnerID, uint8_t aOwnerSlot)
+    {
+        auto spawner = Reg.create();
+        Reg.emplace<Spawner>(spawner);
+        Reg.emplace<Owner>(spawner, aOwnerID, aOwnerSlot);
+        Reg.emplace<Transform3D>(spawner, glm::vec3(5.0f, 0.0f, 5.0f));
+        return spawner;
+    }
+
+    entt::entity AddCreep(CreepType aType, float aHealth, PlayerID aLastHitBy = PlayerID{})
+    {
+        auto creep = Reg.create();
+        Reg.emplace<Health>(creep, Health{.Health = aHealth, .LastHitBy = aLastHitBy});
+        Reg.emplace<Creep>(creep, aType, Definitions.Creeps.at(aType).Damage);
+        return creep;
     }
 };
